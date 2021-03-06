4f664552dcece3493c1868171766c4a5c150456c
briasp8210 <briantaipei8210@gmail.com>
2021-03-06 16:55:36 +0800

    test: Add test data into database

--- schema.sql
+++ schema.sql
@@ -22,3 +22,14 @@ CREATE TABLE messages (
     data_type message_data_t NOT NULL,
     contents varchar(512) NOT NULL
 );
+
+INSERT INTO users (username, password) VALUES ('a', decode('697f2d856172cb8309d6b8b97dac4de344b549d4dee61edfb4962d8698b7fa803f4f93ff24393586e28b5b957ac3d1d369420ce53332712f997bd336d09ab02a', 'hex'));
+INSERT INTO users (username, password) VALUES ('b', decode('8446c46ee03793ba6e5813ba0db4480008926dd1d19efe2c8eb92f9034da974d2171ae483f29ce3a79ed4fdd621ae1ed14fe12532af95ddd0728779ce5aa842d', 'hex'));
+INSERT INTO users (username, password) VALUES ('c', decode('bfe4d7f7377116dc15f794d902621797b72b32396382de2b6e49d4f1d7eabdfddcfc3bc127bb67f92f9458a5733bb21804e7ccd56b4b6f81049339f477cd279d', 'hex'));
+INSERT INTO conversations (name, type, member_ids) VALUES ('A->B', 'PRIVATE', '{1, 2}');
+INSERT INTO conversations (name, type, member_ids) VALUES ('A->C', 'PRIVATE', '{1, 3}');
+INSERT INTO messages (sender_id, conversation_id, data_type, contents) VALUES (1, 1, 'TEXT', 'Hello B, I am A');
+INSERT INTO messages (sender_id, conversation_id, data_type, contents) VALUES (1, 1, 'TEXT', 'This is a direct message');
+INSERT INTO messages (sender_id, conversation_id, data_type, contents) VALUES (2, 1, 'TEXT', 'Hi~ message received');
+INSERT INTO messages (sender_id, conversation_id, data_type, contents) VALUES (1, 1, 'TEXT', 'Cool');
+INSERT INTO messages (sender_id, conversation_id, data_type, contents) VALUES (1, 2, 'TEXT', 'Hi! C, I am A');
